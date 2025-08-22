a4aefec8ab49b383d6679e00ec6ed68264aa6798
Moritz Pabst <moritz.pabst@stud.uni-goettingen.de>
2025-08-22 15:08:36 +0200

    Changed setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
    'pyDOE2>=1.3.0',
    'GPy>=1.9.9'
  ],
-  scripts=["bin/app-ls", "bin/app-tune2", "bin/app-build", "bin/app-predict", "bin/app-datadirtojson", "bin/app-yoda2h5", "bin/app-yodaenvelope", "bin/app-sample", "etc/extrema.py"],
+  scripts=["bin/app-ls", "bin/app-tune2", "bin/app-build", "bin/app-predict", "bin/app-datadirtojson", "bin/app-yoda2h5", "bin/app-yodaenvelope", "bin/app-sample", "etc/extrema.py", "bin/app-bilevel"],
   extras_require = {
   },
   entry_points = {
